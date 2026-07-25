f816d1fc09f6ddc8119b3ef5a31a98ee3ee5724b
youcantemir <youcant@ukr.net>
2026-07-25 18:27:48 +0300

    Add common passwords to common_passwords.py

---
+++ data/common_passwords.py
@@ -0,0 +1,21 @@
+COMMON_PASSWORDS = {
+
+    "123456",
+
+    "password",
+
+    "qwerty",
+
+    "admin",
+
+    "welcome",
+
+    "111111",
+
+    "abc123",
+
+    "letmein",
+
+    "123456789"
+
+}
