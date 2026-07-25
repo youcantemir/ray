25cf4434ba293623da6c627b309ce9e570e45813
youcantemir <youcant@ukr.net>
2026-07-25 18:24:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-""" Password strength checker """
+from repository.password_repository import PasswordRepository
+from services.password_service import PasswordService
+from services.report_service import ReportService
+from exporters.text_exporter import TextExporter
 
-print("Project: password-checker")
+from config import REPORT_FILE
+
+password = PasswordRepository().load()
+
+result = PasswordService().analyze(
+
+    password
+
+)
+
+ReportService().print(
+
+    result
+
+)
+
+TextExporter().export(
+
+    result,
+
+    REPORT_FILE
+
+)
